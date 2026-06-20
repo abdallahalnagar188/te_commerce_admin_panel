a37a516e924464836e16cb9bddd5750e5480c002
Abdallah Alnajar <140744532+abdallahalnagar188@users.noreply.github.com>
2026-06-21 00:04:35 +0300

    z

--- test_auth.dart
+++
@@ -1,6 +0,0 @@
-import 'package:googleapis_auth/auth.dart';
-
-void main() async {
-  final credentials = ServiceAccountCredentials.fromJson({});
-  final client = await clientViaServiceAccount(credentials, []);
-}
